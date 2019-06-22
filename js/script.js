442b697e87054ce93c55354c409999b219bcf432
Kirsty Morris <morriskirsty@gmail.com>
2019-06-22 23:33:58 +0100

    Added css and js for nav

---
+++ js/script.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function($){
+    // Toggle nav menu on button click
+    var $root = $('html, body'),
+        menu_toggle = $('.menu-toggle');
+
+    menu_toggle.on('click', function(){
+        var $this = $(this);
+
+        $this.toggleClass('close');
+        $('.menu, .menu-nav, .menu-nav-item, .submenu-nav, .submenu-nav-item').toggleClass('show');
+    });
+});
