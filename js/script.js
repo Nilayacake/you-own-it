a0e58c61f46786639329beebdbed7517c64aea78
Kirsty Morris <morriskirsty@gmail.com>
2019-06-23 19:34:38 +0100

    Fixed jquery url

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ jQuery(document).ready(function($){
         var $this = $(this);
 
         $this.toggleClass('close');
-        $('.menu, .menu-nav, .menu-nav-item, .submenu-nav, .submenu-nav-item').toggleClass('show');
+        $('.menu, .menu-nav, .menu-nav-item').toggleClass('show');
     });
 
     // Toggle nav menu on menu item click and smooth scrolling
@@ -23,38 +23,40 @@ jQuery(document).ready(function($){
         $root.animate({
             scrollTop: $(href).offset().top - 100
         }, 500, function() {
-            window.location.hash = href;   
+            window.location.hash = href;
         });
 
         return false;
     });
 
     // Attach a submit handler to the form
-    $(".contact-form").submit(function(event) {
+    $(".contact-form" ).submit(function(event) {
+        
         // Stop form from submitting normally
         event.preventDefault();
-
-        // Serialize submitting form data and get action
+        
+        // Serialize submitted form data and get action
         var $form = $(this),
-            url = $form.attr('action');
-
-        // Send data using post
-        var posting = $.post(url, $form.seralize());
-
-        // Put results in a div
+        url = $form.attr("action");
+        
+        // Send the data using post
+        var posting = $.post(url, $form.serialize());
+        
+        // Put the results in a div
         posting.done(function(data) {
+            // Parse response
             var response = $.parseJSON(data),
-                target = $('#status-message');
-
-            // Add success/error classes
-            if(response.status == 1) {
+                target = $('#status-messages');
+            
+            // Add error/success classes
+            if (response.status == 1) {
                 target.removeClass('error');
                 target.addClass('success');
             } else if (response.status == 0) {
                 target.removeClass('success');
                 target.addClass('error');
             }
-
+            
             // Append message
             target.empty().append(response.message).hide().fadeIn(400);
         });
