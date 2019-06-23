006bc3bd7ef24ae7335d99913c54ac1d626c30d8
Kirsty Morris <morriskirsty@gmail.com>
2019-06-23 04:06:37 +0100

    Added jquery for nav, added footer

--- js/script.js
+++ js/script.js
@@ -10,6 +10,25 @@ jQuery(document).ready(function($){
         $('.menu, .menu-nav, .menu-nav-item, .submenu-nav, .submenu-nav-item').toggleClass('show');
     });
 
+    // Toggle nav menu on menu item click and smooth scrolling
+    $('.menu-nav-item a').on('click', function() {
+        var $this = $(this),
+            href = $this.attr('href');
+
+        // Toggle nav menu
+        menu_toggle.removeClass('close');
+        $this.parents('.menu-nav-item, .menu-nav, .menu').removeClass('show');
+
+        // Smooth scrolling
+        $root.animate({
+            scrollTop: $(href).offset().top - 100
+        }, 500, function() {
+            window.location.hash = href;   
+        });
+
+        return false;
+    });
+
     // Attach a submit handler to the form
     $(".contact-form").submit(function(event) {
         // Stop form from submitting normally
