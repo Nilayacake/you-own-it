51c27031c48dc360434d7233034a48bcdea27ce0
Kirsty Morris <morriskirsty@gmail.com>
2019-06-23 03:50:34 +0100

    Added jquery for form

--- js/script.js
+++ js/script.js
@@ -9,4 +9,35 @@ jQuery(document).ready(function($){
         $this.toggleClass('close');
         $('.menu, .menu-nav, .menu-nav-item, .submenu-nav, .submenu-nav-item').toggleClass('show');
     });
+
+    // Attach a submit handler to the form
+    $(".contact-form").submit(function(event) {
+        // Stop form from submitting normally
+        event.preventDefault();
+
+        // Serialize submitting form data and get action
+        var $form = $(this),
+            url = $form.attr('action');
+
+        // Send data using post
+        var posting = $.post(url, $form.seralize());
+
+        // Put results in a div
+        posting.done(function(data) {
+            var response = $.parseJSON(data),
+                target = $('#status-message');
+
+            // Add success/error classes
+            if(response.status == 1) {
+                target.removeClass('error');
+                target.addClass('success');
+            } else if (response.status == 0) {
+                target.removeClass('success');
+                target.addClass('error');
+            }
+
+            // Append message
+            target.empty().append(response.message).hide().fadeIn(400);
+        });
+    });
 });
